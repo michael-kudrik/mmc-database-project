27946793a00ee65df2533ac2f3bec16e41e0ce87
michaelkudrik <mikethesurfer2@gmail.com>
2025-05-01 15:59:46 -0400

    finished backend for query1

--- mmc-app/pages/api/sales/by-price.js
+++ mmc-app/pages/api/sales/by-price.js
@@ -18,4 +18,26 @@ const db = await mysql.createPool({
 
 export default async function handler(req, res){
     const {price, data} = req.query;
+
+    if(!price || !date){
+      return res.status(400).json({ error: "Missing parameter"});
+
+    }
+
+    try{
+      const [rows] = await db.execute(
+        `SELECT e.fname, e.lname, c.make, c.model, t.retail, t.buy_date
+       FROM car_inventory c
+       JOIN c_transeid t ON c.c_id = t.cid
+       JOIN emps e ON t.eid = e.EID
+       WHERE DATE(t.buy_date) = ? AND t.retail > ?
+       ORDER BY t.buy_price`,
+       [date, price]
+      );
+
+      res.status(200).json(rows);
+    } catch (err){
+      console.error(err);
+      res.status(500).json({error: "Query was FUBAR", details: err.message});
+    }
 }
